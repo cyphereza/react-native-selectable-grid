c9f0fc2f59661d395a125b61f04febcdabfab60d
Unknown <cyphereza@gmail.com>
2018-05-23 15:45:26 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # react-native-selectable-grid
+
+![untitled](https://user-images.githubusercontent.com/31456191/40407438-095c5a06-5e97-11e8-8892-9e91ffade2de.gif)
+
+Customizeable selectable (none, one or more) grid.
+
+### Installation
+1. Run: `npm install react-native-selectable-grid --save`
+
+### Usage (Example)
+```javascript
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import SelectableGrid from 'react-native-selectable-grid'
+
+const fakeData = [{ a: '1' }, { a: '2' }, { a: '3' }, { a: '4' }, { a: '5' }];
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <SelectableGrid
+          data={fakeData}
+        />
+      </View>
+    );
+  }
+}
+```
+
+### Properties
+|Prop|Type|Description|Default|
+|----|----|-----------|-------|
+|`data`|array|Receives array of data to be displayed|By default receives array of objects with 'a' key|
+|`maxPerRow`|number|Maximum boxes per row|2|
+|`maxSelect`|number|Number of selectable boxes|1|
+|`customRender`|function|Custom item (box) component|_None_|
+|`unselectedStyle`|style|Style for unselected boxes|_None_|
+|`selectedStyle`|style|Style for selected boxes|_None_|
+
+### Custom Render
+E.g:
+```javascript
+customRender={data => (
+  <View>
+    <Text style={{ color: 'red', fontSize: 35 }}>{data.a}</Text>
+  </View>)}
+```
---
+++ index.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const renderBox = (item = { a: 'something' }) => (
+  <Text style={[{ color: 'white', fontSize: 20 }]}>{item.a}</Text>
+);
+
+
+class SelectableGrid extends Component {
+  state = {
+    selectedItem: -1,
+    itemsArray: [],
+    width: 0,
+    height: 0,
+  };
+
+  onPageLayout = (event) => {
+    const { width, height } = event.nativeEvent.layout;
+    this.setState({ width, height });
+  };
+
+  getIndex = (value, arr) => {
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+    return -1; // to handle the case where the value doesn't exist
+  }
+
+  selectedData = () => {
+    const { data, maxSelect } = this.props;
+
+    if (maxSelect === 1) {
+      return data[this.state.selectedItem];
+    } else if (maxSelect > 1) {
+      const selectedDataArray = [];
+
+      for (let i = 0; i < this.state.itemsArray.length; i += 1) {
+        selectedDataArray.push(data[this.state.itemsArray[i]]);
+      }
+
+      return selectedDataArray;
+    }
+  };
+
+  calculateBoxWidth = maxPerRow => (this.state.width / maxPerRow);
+
+  handleSelectItem = (keyValue) => {
+    const { maxSelect } = this.props;
+
+    if (maxSelect === 1) {
+      if (this.state.selectedItem === keyValue) {
+        this.setState({ selectedItem: -1 });
+      } else {
+        this.setState({ selectedItem: keyValue });
+      }
+    } else if (maxSelect > 1) {
+      const newItemsArray = this.state.itemsArray;
+
+      // Check if keyValue already exist in array
+      const itemIndex = newItemsArray.length > 0 ? this.getIndex(keyValue, newItemsArray) : -1;
+
+      // If item found in array
+      if (itemIndex > -1) {
+        newItemsArray.splice(itemIndex, 1);
+      } else if (this.state.itemsArray.length < maxSelect) {
+        newItemsArray.push(keyValue);
+      }
+
+      this.setState({ itemsArray: newItemsArray });
+    }
+  }
+
+  render() {
+    const { data, maxPerRow, customRender, selectedStyle, unselectedStyle } = this.props;
+    const boxWidth = this.calculateBoxWidth(maxPerRow);
+    let counter = 0;
+
+    return (
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }} onLayout={this.onPageLayout}>
+        {data.map((i) => {
+          const keyValue = counter;
+          counter += 1;
+
+          return (
+            <TouchableOpacity style={[styles.contentBox, unselectedStyle, (this.state.selectedItem === keyValue || this.state.itemsArray.includes(keyValue)) && selectedStyle, { height: boxWidth, width: boxWidth }]} key={keyValue} onPress={() => this.handleSelectItem(keyValue)}>
+              {customRender(i)}
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  contentBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#adadad',
+  },
+  contentBoxSelected: {
+    backgroundColor: '#ff6600',
+  },
+});
+
+SelectableGrid.propTypes = {
+  data: PropTypes.array.isRequired,
+  maxPerRow: PropTypes.number,
+  maxSelect: PropTypes.number,
+  customRender: PropTypes.func,
+  unselectedStyle: PropTypes.any,
+  selectedStyle: PropTypes.any,
+};
+
+SelectableGrid.defaultProps = {
+  maxPerRow: 2,
+  maxSelect: 1,
+  customRender: renderBox,
+  unselectedStyle: {},
+  selectedStyle: styles.contentBoxSelected,
+};
+
+export default SelectableGrid;
