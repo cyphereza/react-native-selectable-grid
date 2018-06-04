fd31292f3b4f8b535fc5824b652258d518e1cb98
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2018-06-04 16:55:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const renderBox = (item = { a: 'something' }) => (
-  <Text style={[{ color: 'white', fontSize: 20 }]}>{item.a}</Text>
+const renderBox = (item = { label: 'something' }) => (
+  <Text style={[{ color: 'white', fontSize: 20 }]}>{item.label}</Text>
 );
-
 
 class SelectableGrid extends Component {
   state = {
@@ -15,11 +14,6 @@ class SelectableGrid extends Component {
     height: 0,
   };
 
-  onPageLayout = (event) => {
-    const { width, height } = event.nativeEvent.layout;
-    this.setState({ width, height });
-  };
-
   getIndex = (value, arr) => {
     for (let i = 0; i < arr.length; i += 1) {
       if (arr[i] === value) {
@@ -27,70 +21,102 @@ class SelectableGrid extends Component {
       }
     }
     return -1; // to handle the case where the value doesn't exist
-  }
+  };
 
   selectedData = () => {
     const { data, maxSelect } = this.props;
+    const { itemsArray, selectedItem } = this.state;
 
     if (maxSelect === 1) {
-      return data[this.state.selectedItem];
+      return data[selectedItem];
     } else if (maxSelect > 1) {
       const selectedDataArray = [];
 
-      for (let i = 0; i < this.state.itemsArray.length; i += 1) {
-        selectedDataArray.push(data[this.state.itemsArray[i]]);
+      for (let i = 0; i < itemsArray.length; i += 1) {
+        selectedDataArray.push(data[itemsArray[i]]);
       }
 
       return selectedDataArray;
     }
   };
 
-  calculateBoxWidth = maxPerRow => (this.state.width / maxPerRow);
+  calculateBoxWidth = maxPerRow => this.state.width / maxPerRow;
 
   handleSelectItem = (keyValue) => {
     const { maxSelect } = this.props;
+    const { selectedItem, itemsArray } = this.state;
 
     if (maxSelect === 1) {
-      if (this.state.selectedItem === keyValue) {
-        this.setState({ selectedItem: -1 });
-      } else {
-        this.setState({ selectedItem: keyValue });
-      }
+      this.setState({
+        selectedItem: selectedItem === keyValue ? -1 : keyValue,
+      });
     } else if (maxSelect > 1) {
-      const newItemsArray = this.state.itemsArray;
+      const newItemsArray = itemsArray;
 
       // Check if keyValue already exist in array
-      const itemIndex = newItemsArray.length > 0 ? this.getIndex(keyValue, newItemsArray) : -1;
+      const itemIndex =
+        newItemsArray.length > 0 ? this.getIndex(keyValue, newItemsArray) : -1;
 
       // If item found in array
       if (itemIndex > -1) {
         newItemsArray.splice(itemIndex, 1);
-      } else if (this.state.itemsArray.length < maxSelect) {
+      } else if (itemsArray.length < maxSelect) {
         newItemsArray.push(keyValue);
       }
 
       this.setState({ itemsArray: newItemsArray });
     }
-  }
+  };
 
   render() {
-    const { data, maxPerRow, unselectedRender, selectedRender, selectedStyle, unselectedStyle } = this.props;
-    const boxWidth = this.calculateBoxWidth(maxPerRow);
-    let counter = 0;
+    const {
+      data,
+      maxPerRow,
+      unselectedRender,
+      selectedRender,
+      selectedStyle,
+      unselectedStyle,
+    } = this.props;
+    const { selectedItem, itemsArray } = this.state;
+
+    const content = [];
+    for (let i = 0, counter = 0; i < data.length / maxPerRow; i++) {
+      const row = [];
+      for (let k = 0; k < maxPerRow; k++) {
+        const keyValue = counter;
+        if (counter < data.length) {
+          row.push(
+            <TouchableOpacity
+              style={[
+                styles.contentBox,
+                unselectedStyle,
+                (selectedItem === keyValue || itemsArray.includes(keyValue)) &&
+                  selectedStyle,
+                { height: 100, flex: 1 },
+              ]}
+              onPress={() => this.handleSelectItem(keyValue)}
+            >
+              {selectedItem === keyValue || itemsArray.includes(keyValue)
+                ? selectedRender(data[keyValue])
+                : unselectedRender(data[keyValue])}
+            </TouchableOpacity>,
+          );
+        } else {
+          row.push(
+            <View style={{ flex: 1, backgroundColor: 'transparent' }} />,
+          );
+        }
+        counter++;
+      }
+      content.push(
+        <View style={{ flexDirection: 'row', width: '100%', height: 100 }}>
+          {row}
+        </View>,
+      );
+    }
 
     return (
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }} onLayout={this.onPageLayout}>
-        {data.map((i) => {
-          const keyValue = counter;
-          counter += 1;
-
-          return (
-            <TouchableOpacity style={[styles.contentBox, unselectedStyle, (this.state.selectedItem === keyValue || this.state.itemsArray.includes(keyValue)) && selectedStyle, { height: boxWidth, width: boxWidth }]} key={keyValue} onPress={() => this.handleSelectItem(keyValue)}>
-              {(this.state.selectedItem === keyValue || this.state.itemsArray.includes(keyValue)) ? selectedRender(i) : unselectedRender(i)}
-            </TouchableOpacity>
-          );
-        })}
-      </View>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>{content}</View>
     );
   }
 }
